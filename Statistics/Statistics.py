e37b5278697520eb22e07f8467b10705081f57fe
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-15 23:05:08 -0500

    Fix: Added mean.py, mean.csv and statistics file

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,21 +1,8 @@
-import unittest
-from Calculator.Calculator import Calculator
-from Statistics.Statistics import Statistics
-from CsvReader.CsvReader import CsvReader
+from Statistics.Mean import mean
 
+class Statistics:
+    data = []
 
-class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.statistics = Statistics()
-
-
-    def test_mean(self):
-        test_data = CsvReader("/Tests/Data/Mean.csv").data
-        for row in test_data:
-            self.assertEqual(self.statistics.mean(row['Value 1'], row['Value 2']), float(row['Result']))
-            self.assertEqual(self.statistics.result, float(row['Result']))
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def mean(self, a, b):
+        self.result = mean(a, b)
+        return self.result
---
+++ Tests/test_statistics.py
@@ -0,0 +1,19 @@
+import unittest
+from Calculator.Calculator import Calculator
+from Statistics.Statistics import Statistics
+from CsvReader.CsvReader import CsvReader
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.statistics = Statistics()
+
+
+    def test_mean(self):
+        test_data = CsvReader("/Tests/Data/Mean.csv").data
+        for row in test_data:
+            self.assertEqual(self.statistics.mean(row['Value 1'], row['Value 2']), float(row['Result']))
+            self.assertEqual(self.statistics.result, float(row['Result']))
+
+if __name__ == '__main__':
+    unittest.main()
