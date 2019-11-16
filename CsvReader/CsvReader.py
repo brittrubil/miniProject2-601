03e96f2f124967884213f0805f64c0ddc72d46a8
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-15 22:30:47 -0500

    Task: Added CsvReader, init and test_calculator files

---
+++ CsvReader/CsvReader.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+
+
+def classfactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
+
+class CsvReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+        with open(absolutepath(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+                pprint(self.data)
+        pass
+
+    def return_data_as_objects(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(classfactory(class_name, row))
+        return objects
